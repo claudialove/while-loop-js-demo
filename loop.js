32eeb2fa4278c21296b0025be9d7af35d3e1ad1e
Claudia Petty <claudialove32@gmail.com>
2020-06-15 18:31:37 -0400

    a single js file dmonstrating the use of while loops printing numbers to the console based on various conditions

---
+++ loop.js
@@ -0,0 +1,30 @@
+
+//print all numbers between -10 and 19
+var counter = -10;
+while (counter < 20) {
+    console.log(counter);
+counter ++
+};
+console.log("break");
+//print all even numbers between 10 and 40
+var counter = 10;
+while (counter < 41) {
+    console.log(counter);
+    counter ++
+};
+console.log("break");
+//print all odd numbers between 300 and 333
+var counter = 300;
+while (counter < 334) {
+    console.log(counter);
+    counter++
+};
+console.log("break");
+//print all numbers divsible by 5 and 3 between 5 and 50
+var counter = 5;
+while (counter < 51) {
+    if (counter %5 ===0 && counter%3 ===0);
+    console.log (counter)
+    counter ++;
+}
+
